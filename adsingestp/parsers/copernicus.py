91750a0bc640d8beec50eadc6e0f6b671ad8e940
Mugdha Polimera <mugdhapolimera@saos-mbp.lan>
2023-09-18 17:29:44 -0400

    copernicus lint edits

--- adsingestp/parsers/copernicus.py
+++ adsingestp/parsers/copernicus.py
@@ -31,7 +31,6 @@ class CopernicusParser(BaseBeautifulSoupParser):
         "remove_the": False,
     }
 
-
     def __init__(self):
         self.base_metadata = {}
         self.input_header = None
@@ -49,14 +48,13 @@ class CopernicusParser(BaseBeautifulSoupParser):
             if journal_metadata.find("volume_number"):
                 self.base_metadata["volume"] = journal_metadata.find("volume_number").get_text()
 
-
     def _parse_pagination(self):
         if self.input_metadata.find("start_page"):
             self.base_metadata["page_first"] = self.input_metadata.find("start_page").get_text()
 
         if self.input_metadata.find("end_page"):
             self.base_metadata["page_last"] = self.input_metadata.find("end_page").get_text()
-    
+
         if self.record_meta.find("article_number"):
             self.base_metadata["electronic_id"] = self.input_metadata.find(
                 "article_number"
@@ -71,8 +69,8 @@ class CopernicusParser(BaseBeautifulSoupParser):
 
         if self.input_metadata.find("eissn"):
             issns.append(("electronic", self.input_metadata.find("issn").get_text()))
-            
-        self.base_metadata["issn"]  = issns
+        
+        self.base_metadata["issn"] = issns
 
         if self.input_metadata.find("doi"):
             self.base_metadata["ids"]["doi"] = self.input_metadata.find("doi").get_text()
@@ -92,21 +90,21 @@ class CopernicusParser(BaseBeautifulSoupParser):
                 title = title_temp.get_text().title()
 
                 self.base_metadata["title"] = title
-                
+
         else:
             raise MissingTitleException("No title found")
 
     def _parse_author(self):
         author_list = []
         name_parser = utils.AuthorNames()
-        
+
         affil_map = {}
-        
+
         # Create a dictionary to map affiliation names to affiliation numbers
         if self.input_metadata.find_all("affiliations"):
             affil_list = self.input_metadata.find_all("affiliations")[0].find_all("affiliation")
             for aff in affil_list:
-                affil_map[aff.get("numeration","")] = self._clean_output(aff.get_text())
+                affil_map[aff.get("numeration", "")] = self._clean_output(aff.get_text())
 
         author_array = self.input_metadata.find_all("author")
         for a in author_array:
@@ -115,13 +113,13 @@ class CopernicusParser(BaseBeautifulSoupParser):
             parsed_name = name_parser.parse(
                 name, collaborations_params=self.author_collaborations_params
             )
-            author_temp = parsed_name[0]        
-            
+           author_temp = parsed_name[0]
+
             if a.find("email"):
                 author_temp["email"] = a.find("email").get_text()
             if a.find("contrib-id"):
                 author_temp["orcid"] = a.find("contrib-id").get_text()
-                
+
             if a["affiliations"]:
                 aff_temp = []
                 for author_affil in str(a["affiliations"]).split(",") :
@@ -130,19 +128,17 @@ class CopernicusParser(BaseBeautifulSoupParser):
                                 
                 
             author_list.append(author_temp)
-            
+
         if not author_list:
             raise MissingAuthorsException("No contributors found for")
 
-        self.base_metadata["authors"] = author_list
-        
+        self.base_metadata["authors"] = author_list        
 
     def _parse_pubdate(self):
         if self.input_metadata.find("publication_date"):
             self.base_metadata["pubdate_electronic"] = self.input_metadata.find(
                 "publication_date"
             ).get_text()
-        
 
     def _parse_abstract(self):
         abstract = None
@@ -157,11 +153,10 @@ class CopernicusParser(BaseBeautifulSoupParser):
         if abstract:
             self.base_metadata["abstract"] = self._clean_output(abstract)
 
-
     def _parse_references(self):
         if self.input_metadata.find("references") and self.input_metadata.find("references").find(
-                "reference"
-            ):
+            "reference"
+        ):
             references = []
             for ref in self.input_metadata.find("references").find_all("reference"):
                 # output raw XML for reference service to parse later
@@ -176,7 +171,7 @@ class CopernicusParser(BaseBeautifulSoupParser):
             links.append(("pub_pdf", self.input_metadata.find("fulltext_pdf").get_text()))
         if self.input_metadata.find("abstract_html"):
             links.append(("pub_html", self.input_metadata.find("abstract_html").get_text()))
-        
+
         self.base_metadata["esources"] = links
 
     def parse(self, text):
