0c749b5a9b98ba9cd035c9d73a3383fa8b4eeb8d
Mugdha Polimera <mugdhapolimera@saos-MacBook-Pro.local>
2023-09-21 11:14:29 -0400

    Revert "jats modification to handle springer"
    This reverts commit 730e51827ba950f43db790bab2ab56c6600e431f.

--- adsingestp/parsers/jats.py
+++ adsingestp/parsers/jats.py
@@ -9,9 +9,6 @@ from adsingestp import utils
 from adsingestp.ingest_exceptions import XmlLoadException
 from adsingestp.parsers.base import BaseBeautifulSoupParser
 
-from copy import copy
-
-import pdb
 logger = logging.getLogger(__name__)
 
 
@@ -188,76 +185,39 @@ class JATSAffils(object):
             art_contrib_groups = article_metadata.find_all("contrib-group")
 
         authors_out = []
-        contribs_out = []        
+        contribs_out = []
 
         # JATS puts author data in <contrib-group>, giving individual authors in each <contrib>
         for art_group in art_contrib_groups:
             art_contrib_group = art_group.extract()
-
-
-        # extract <contrib> from each <contrib-group>
-            contribs_raw_init = []
-            if art_contrib_group.find_all("contrib", recursive = False):
-                contribs_raw_init = art_contrib_group.find_all("contrib", recursive = False)
-
+            contribs_raw = art_contrib_group.find_all("contrib")
             default_key = "ALLAUTH"
-
-        # cycle through <contrib> to check if a <collab> is listed in the same level as an author an has multiple authors nested under it; targeted for Springer            
-            contribs_raw = []
-            collabflag = None
-            for i in range(len(contribs_raw_init)): 
-                if contribs_raw_init[i].find('collab'): 
-                    # find nested collab authors and unnest them 
-                    if contribs_raw_init[i].find('collab').find('institution'):                         
-                        # save the author position of collab to add collab as an author later
-                        collabflag = i
-                        collab_authors = contribs_raw_init[i].find_all('contrib')
-                        # add new collab tag to each unnested author 
-                        for ca in collab_authors:
-                            collabtag = copy(contribs_raw_init[collabflag].find('collab').find('institution'))
-                            ca.append(collabtag)
-                            contribs_raw.append(ca)
-                    else:
-                        contribs_raw.append(contribs_raw_init[i])
-
-                else:
-                    contribs_raw.append(contribs_raw_init[i])
-
             num_contribs = len(contribs_raw)
-            count = 0
             for contrib in contribs_raw:
                 # note: IOP, APS get affil data within each contrib block,
                 #       OUP, AIP, Springer, etc get them via xrefs.
-                auth = {}
-                count+=1
-                collab = contrib.find("collab")
 
-                #Springer collab info for nested authors is given as <institution>
-                if not collab:
-                    collab = contrib.find("institution")
-                        
+                auth = {}
+
+                collab = contrib.find("collab")
                 if collab:
-                    collab_affil = ""
                     collab_name = collab.get_text()
                     if collab.find("address"):
                         collab_affil = collab.find("address").get_text()
-
+                    else:
+                        collab_affil = ""
                     self.collab = {
-                        "collab": collab_name,
+                        "name": collab_name,
                         "aff": collab_affil,
                         "xaff": [],
                         "xemail": [],
                         "email": [],
                         "corresp": False,
                     }
-                    
-                    
+
                 l_correspondent = False
-                try:
-                    if contrib.get("corresp", "") == "yes":
-                        l_correspondent = True
-                except:
-                    l_correspondent = False
+                if contrib.get("corresp", "") == "yes":
+                    l_correspondent = True
 
                 # get author's name
                 if contrib.find("name") and contrib.find("name").find("surname"):
@@ -349,9 +309,7 @@ class JATSAffils(object):
                 auth["xemail"] = xref_email
                 auth["orcid"] = orcid_out
                 auth["email"] = email_list
-                if collab: 
-                    auth["collab"] = collab_name
-                    
+
                 # this is a list of author dicts
                 if auth:
                     if contrib.get("contrib-type", "author") == "author":
@@ -365,13 +323,11 @@ class JATSAffils(object):
                         auth["role"] = role
                         contribs_out.append(auth)
                         default_key = "ALLCONTRIB"
+
                 contrib.decompose()
 
             if self.collab:
-                #add collab in the correct author position
-                if collabflag:
-                    authors_out.insert(collabflag, self.collab)
-                    
+                authors_out.append(self.collab)
 
             # special case: affs defined in contrib-group, but not in individual contrib
             if art_contrib_group:
--- tests/test_jats.py
+++ tests/test_jats.py
@@ -27,18 +27,7 @@ class TestJATS(unittest.TestCase):
         self.maxDiff = None
 
     def test_jats(self):
-
         filenames = [
-            'jats_SpringerEarly_ExA_s10686-023-09907-7', 
-            'jats_Springer_EPJC_s10052-023-11699-1',
-            'jats_Springer_Natur_s41598-023-38673-x',
-            'jats_Springer_AcMSn_s10409-023-23061-x',
-            'jats_Springer_EPJC_s10052-023-11733-2',
-            'jats_Springer_ZaMP_s00033-023-02064-z',
-            'jats_Springer_AcMSn_s10409-023-23086-x',
-            'jats_Springer_JHEP_JHEP07_2023_200',
-            'jats_Springer_AcMSn_s10409-023-23108-x',
-            'jats_Springer_NatCo_s41467-023-40272-3',
             "jats_apj_859_2_101",
             "jats_mnras_493_1_141",
             "jats_aj_158_4_139",
@@ -52,21 +41,20 @@ class TestJATS(unittest.TestCase):
             "jats_iop_preprint_in_record",
             "jats_iop_apj_923_1_47",
             "jats_a+a_multiparagraph_abstract",
-            ]
-
+        ]
         for f in filenames:
             test_infile = os.path.join(self.inputdir, f + ".xml")
             test_outfile = os.path.join(self.outputdir, f + ".json")
             parser = jats.JATSParser()
-            
+
             with open(test_infile, "rb") as fp:
                 input_data = fp.read()
-
-            parsed = parser.parse(input_data)
 
             with open(test_outfile, "rb") as fp:
                 output_text = fp.read()
                 output_data = json.loads(output_text)
+
+            parsed = parser.parse(input_data)
 
             # make sure this is valid schema
             try:
@@ -161,4 +149,4 @@ class TestJATS(unittest.TestCase):
                 output_text = fp.read()
                 output_data_tags = json.loads(output_text)
             cite_context = parser.citation_context(input_data, text_output=False)
-            # self.assertEqual(cite_context, output_data_tags)
+            self.assertEqual(cite_context, output_data_tags)
